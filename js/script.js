afcde46da3a30cd9572736a97b7e4639d18b58bc
Gev <gevbabloyan566@gmail.com>
2024-08-13 12:48:07 +0400

    fix accordion delete image oc

--- js/script.js
+++ js/script.js
@@ -652,22 +652,28 @@
 	//Accordion Box
 	if($('.accordion-box').length){
 		$(".accordion-box").on('click', '.acc-btn', function() {
-			
+
 			var outerBox = $(this).parents('.accordion-box');
 			var target = $(this).parents('.accordion');
 			
 			if($(this).hasClass('active')!==true){
 				$(outerBox).find('.accordion .acc-btn').removeClass('active');
 			}
-			
+
 			if ($(this).next('.acc-content').is(':visible')){
+				if ($(outerBox)[0].classList.value.includes( 'vacancy-accordion')) {
+					$(this).removeClass('active');
+					$(outerBox).children('.accordion').removeClass('active-block');
+					$(outerBox).find('.accordion').children('.acc-content').slideUp(300);
+				}
+
 				return false;
 			}else{
 				$(this).addClass('active');
 				$(outerBox).children('.accordion').removeClass('active-block');
 				$(outerBox).find('.accordion').children('.acc-content').slideUp(300);
 				target.addClass('active-block');
-				$(this).next('.acc-content').slideDown(300);	
+				$(this).next('.acc-content').slideDown(300);
 			}
 		});	
 	}
